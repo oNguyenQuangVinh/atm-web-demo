0dc06aee09a720cba1b9405f05326bab370364e7
truong.t.phuc@gmail.com <truong.t.phuc@gmail.com@02d4df25-923b-04b6-c80e-39c58ff455dc>
2015-05-03 14:14:08 +0000

    -- Phuc -- Commit lai project source code

--- trunk/Source/ATMSystemGroup4/Backup/ATMSystemGroup4/Global.asax.cs
+++ trunk/Source/ATMSystemGroup4/Backup/ATMSystemGroup4/Global.asax.cs
@@ -7,7 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace ATMSystem
+namespace ATMSystemGroup4
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
